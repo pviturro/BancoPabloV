ba6cd92e81b42f275d52649dc8d051ad4165b547
Pablo Viturro <pviturro@nettrim.es>
2021-09-02 12:37:13 +0200

    Agregar archivos de proyecto.

---
+++ BancoPabloV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BancoPabloV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
